daba4c412d10c666555aa658bf6ab423c6a591ec
pachon_1992 <pachonunderscore1992@gmail.com>
2017-03-23 09:42:47 -0400

    mover el template a un archivo

--- app/introduccion/component.js
+++ app/introduccion/component.js
@@ -4,20 +4,5 @@ app.component('introduccion', {
   controller: function(IntroduccionService) {
     this.ejemplos = IntroduccionService.getEjemplos();
   },
-  template: `
-    <header class="jumbotron" id="overview">
-      <div class="container">
-        <div class="col-sm-7">
-          <h1>Introduccion</h1>
-        </div>
-      </div>
-    <div ng-repeat="ejemplo in $ctrl.ejemplos">
-      <h2> {{ejemplo.nombre}} </h2>
-      <h3> {{ejemplo.comentario}} </h3>
-      <div hljs hljs-include="ejemplo.url">
-      </div>
-      <a ng-href="{{ejemplo.url}}" download>Descargar</a>
-    </div>
-    </header>
-  `
+  templateUrl: 'app/introduccion/introduccion.html'
 })
